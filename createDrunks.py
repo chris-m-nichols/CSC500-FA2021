1134bbb8941353773f3ae85e12920d4cd67b5430
Chris Nichols <cmnichol@gmail.com>
2021-12-07 16:34:20 -0500

    .

--- createDrunks.py
+++ createDrunks.py
@@ -2,9 +2,12 @@ import random
 
 def createDrunks(width, height):
     colorList = ['red', 'green', 'blue', 'black', 'purple', 'teal', 'yellow']
+    shapes = ['turtle', 'circle', 'square', 'triangle', 'classic']
     ballColor = random.choice(colorList)
     speed = 0
     xcord = random.randint(-width/2, width/2)
     ycord = random.randint(-height/2, height/2)
-    return [ballColor, speed, xcord, ycord]
+    shape = random.choice(shapes)
+    pencolor = random.choice(colorList)
+    return [ballColor, speed, xcord, ycord, shape, pencolor]
 
