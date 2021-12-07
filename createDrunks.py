d197658b002c9e8860fda80d0e5daf24d93b086d
Chris Nichols <cmnichol@gmail.com>
2021-12-07 15:47:37 -0500

    Cleanup

--- createDrunks.py
+++ createDrunks.py
@@ -2,8 +2,6 @@ import random
 
 def createDrunks(width, height):
     colorList = ['red', 'green', 'blue', 'black', 'purple', 'teal', 'yellow']
-    xcords = [-40, 0, 40]
-    ycords = [-40, 0, 40]
     ballColor = random.choice(colorList)
     speed = 0
     xcord = random.randint(-width/2, width/2)
